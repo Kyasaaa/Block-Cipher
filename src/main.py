ec33701523b6f56f25afaa579b5c1b884c0a5e86
GleenMike <rahmatrafid22@gmail.com>
2023-03-05 16:24:31 +0700

    Fix : File doesnt exist exception handler

--- src/main.py
+++ src/main.py
@@ -7,11 +7,21 @@ from encrypt import encrypt
 def cls():
     os.system("cls")
     
-def read_file(filename):
+def read_file(type):
+    filename = input(f"Enter your {type} filename: ")
     dir_path = "Text_files//Test//" + filename + ".txt"
-    f = open(dir_path, "r", encoding=ENCODING)
-    text = f.read()
-    f.close()
+    text = ""
+    found = False
+    while (not(found)):
+        if (os.path.exists(dir_path)):
+            f = open(dir_path, "r", encoding=ENCODING)
+            text = f.read()
+            f.close()
+            found = True
+        else:
+            print("File not found, please try again.")
+            filename = input(f"Enter your {type} filename: ") 
+            dir_path = "Text_files//Test//" + filename + ".txt"
     return text
 
 def write_file(text, filename):
@@ -39,23 +49,29 @@ key_filename  = ""
 cls()
 # 1. Choose the cryptographic mode
 print("-------------------------------------------------------------")
-print("~~~  Welcome to SwazzyCipher..!!!  ~~~")
+print("-------------------------------------------------------------")
+print("~~~~~~~~~~~~~   Welcome to SwazzyCipher...!!!   ~~~~~~~~~~~~~")
+print("-------------------------------------------------------------")
+print("-------------------------------------------------------------")
+print()
 print("What will you like to do?")
 print("1. Encrypt your plaintext.")
 print("2. Decrypt your ciphertext.")
 print()
-cript_op = int(input("Kript Mode: "))
+cript_op = int(input("Choose Kript: "))
 print()
 print("-------------------------------------------------------------")
 match cript_op:
     case 1:
-        print("~~~ You Choose Encryption Mode  ~~~")
+        print("~~~~~~~~~~~~~~~   You Choose Encryption Mode  ~~~~~~~~~~~~~~~")
     case 2:
-        print("~~~ You Choose Decryption Mode ~~~")
+        print("~~~~~~~~~~~~~~~   You Choose Encryption Mode  ~~~~~~~~~~~~~~~")
     case default:
         print("Please type correct number")
         cript_op = int(input("Choose Kript: "))
-        
+print("-------------------------------------------------------------")
+print()
+
 # 2. Choose the input mode
 print("How will you like to input your text and key?")
 print("1. Input by user typing.")
@@ -65,37 +81,40 @@ input_op = int(input("Choose Input: "))
 print()
 match input_op:
     case 1:
-        print("~~~ Input by user typing  ~~~")
+        print("~~~~~~~~~~~~~~~~~~   Input by user typing   ~~~~~~~~~~~~~~~~~~")
         text = input("Enter your text: ")
-        key  = input("Enter your key: ")
+        key  = input("Enter your  key: ")
     case 2:
-        print("~~~ Input by filename (in 'Text_Files/Test' folder)  ~~~")
-        text_filename = input("Enter your text filename: ")
-        key_filename  = input("Enter your  key filename: ")
-        text = read_file(text_filename)
-        key = read_file(key_filename)
+        print("~~~~~   Input by filename (in 'Text_Files/Test' folder)  ~~~~~")
+        text = read_file("text")
+        key  = read_file(" key")
     case default:
         print("Please type correct number")
-        input_op = int(input("Input Mode: "))
+        input_op = int(input("Choose Input: "))
 
 # 3. Start Criptography
 print()
+print("-------------------------------------------------------------")
 match cript_op:
     case 1:
+        print("~~~~~~~~~~~~~~~~~~~~   Start Encryption   ~~~~~~~~~~~~~~~~~~~~")
         print("-------------------------------------------------------------")
-        print("~~~ Start Encryption  ~~~")
+        print()
         result = encrypt(text, key)
         print("Your plaintext is: ", f"'{text}'")
         print("Your       key is: ", f"'{key}'")
         print("Your encrypted is: ", f"'{result}'")
     case 2:
+        print("~~~~~~~~~~~~~~~~~~~~   Start Decryption   ~~~~~~~~~~~~~~~~~~~~")
         print("-------------------------------------------------------------")
-        print("~~~ Start Decryption ~~~")
+        print()
         result = decrypt(text, key)
         print("Your ciphertext is: ", f"'{text}'")
         print("Your        key is: ", f"'{key}'")
         print("Your  decrypted is: ", f"'{result}'")
 print()
+print("-------------------------------------------------------------")
+print("-------------------------------------------------------------")
 
 # 4. Save all the text
 match cript_op:
